aa41fff2e1994594a9029439805709e653a9274f
Mark <markhallett5@outlook.com>
2026-01-29 00:30:49 +0000

    added hero img and styled to match figma design

--- cafe-project/src/pages/Home.jsx
+++ cafe-project/src/pages/Home.jsx
@@ -6,7 +6,14 @@ function Home() {
   return (
     <div className="home-page">
       <Navbar />
-      <h1>Pause & Pour Cafe</h1>
+      <section className="hero">
+        <img src="/Cafe.png" alt="Cafe" />
+        <h1 className="hero-title">
+          Pause &amp; Pour
+          <br />
+          <span className="hero-title-cafe">Cafe</span>
+        </h1>
+      </section>
       <Footer />
     </div>
   );
